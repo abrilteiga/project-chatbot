a5827784dce07332019e0192e335c672db20c004
Abril Teiga <132606305+abrilteiga@users.noreply.github.com>
2026-04-27 22:06:05 -0300

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,8 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI()
-
-def get_embdding(text):
-    response = client.embeddings.create( model = "text-embedding-3-small",
-                                        input=text)
-    return response.data[0].embedding
